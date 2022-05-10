54748ce19e65823fcc095fd872928fedf47b209a
Kevin Munc <muncman@gmail.com>
2022-05-09 23:24:53 -0400

    Initial commit. Plus a git ignore file.

---
+++ FormatAndLintExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FormatAndLintExample
+//
+//  Created by Kevin Munc on 5/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FormatAndLintExample/FormatAndLintExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormatAndLintExampleApp.swift
+//  FormatAndLintExample
+//
+//  Created by Kevin Munc on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct FormatAndLintExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
